ba99acdd2704ec661a94aacc6bce4f8befa1df5f
shayff <39059628+shayff@users.noreply.github.com>
2019-05-02 13:22:59 +0300

    Update Find The Parity Outlier.cs

--- Find The Parity Outlier.cs
+++ Find The Parity Outlier.cs
@@ -17,18 +17,24 @@ public class Kata
     {
       v_IsLookForEven=!isEven(integers[0]);
       for(int i=2;i<integers.Length;i++)
-        {
-         if(isEven(integers[i])==v_IsLookForEven) 
+      {
+         if(isEven(integers[i])==v_IsLookForEven)
+         {
           return integers[i];
-        }
+         }
+       }
     }
     else
     {
     //If first different from second we check the third and decide if return first or second
       if(isEven(integers[0])==isEven(integers[2]))
-        return integers[1];
+      {
+      return integers[1];
+      }
       else
+      {
         return integers[0];
+      }
     } 
   return -1;
   }
