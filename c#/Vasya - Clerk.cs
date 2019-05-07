aa58799748fef6a37abb21dd047001f41b73bd96
shayff <39059628+shayff@users.noreply.github.com>
2019-05-07 11:52:42 +0300

    Create Vasya - Clerk.cs

---
+++ c#/Vasya - Clerk.cs
@@ -0,0 +1,53 @@
+ Name: Shay Pepper
+ Date: 07/05/2019
+ Title: Vasya - Clerk
+ Kata: https://www.codewars.com/kata/vasya-clerk/train/csharp
+ */
+
+using System;
+
+public class Line
+    {
+        public static string Tickets(int[] peopleInLine)
+        { 
+           int Bill25 = 0, Bill50=0,Bill100=0;
+           for(int i=0;i<peopleInLine.Length;++i)
+           {      
+           switch (peopleInLine[i])
+            {
+                case 25:
+                    ++Bill25;
+                    break;            
+                case 50:
+                    if(Bill25>=1)
+                     {
+                        --Bill25;
+                        ++Bill50;
+                    }
+                    else
+                    {
+                      return "NO";
+                    }
+                    break;             
+                case 100:
+                    if(Bill50>=1 && Bill25>=1)
+                      {
+                        --Bill25;
+                        --Bill50;
+                        ++Bill100;
+                      }
+                      else if(Bill25>=3)
+                      {
+                       Bill25-=3;
+                      ++Bill100;
+                      }
+                      else
+                      {
+                      return "NO";
+                      }
+                    break;             
+            }               
+           }
+           return "YES";           
+        }
+    }
