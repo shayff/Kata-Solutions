4c6b25e0f8ce9f3c116a30ab15de27d9b0ed3b47
shayff <39059628+shayff@users.noreply.github.com>
2019-05-19 13:23:39 +0300

    Create sum-of-angles.cs

---
+++ c#/sum-of-angles.cs
@@ -0,0 +1,15 @@
+ /*
+ Name: Shay Pepper
+ Date: 19/05/2019
+ Title: Sum of angles
+ Kata: https://www.codewars.com/kata/sum-of-angles/
+ */
+
+
+public class Kata
+{
+    public static int Angle(int n)
+    {
+        return (n-2) * 180;
+    }
+}
