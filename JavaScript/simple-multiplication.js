36bed1abdec557f298feaf7178d0094cfd239460
shayff <39059628+shayff@users.noreply.github.com>
2019-07-11 15:57:05 +0300

    Create simple-multiplication.js

---
+++ JavaScript/simple-multiplication.js
@@ -0,0 +1,12 @@
+ /*
+ Name: Shay Pepper
+ Date: 11/07/2019
+ Title: Simple multiplication
+ Kata: https://www.codewars.com/kata/simple-multiplication
+ */
+
+
+function simpleMultiplication(number) 
+{
+    return number * (number % 2 ? 9 : 8);
+}
