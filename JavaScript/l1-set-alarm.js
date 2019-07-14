a21b8df241a203608d4380932b41749068d762ed
shayff <39059628+shayff@users.noreply.github.com>
2019-07-14 14:49:54 +0300

    Create l1-set-alarm.js

---
+++ JavaScript/l1-set-alarm.js
@@ -0,0 +1,11 @@
+ /*
+ Name: Shay Pepper
+ Date: 14/07/2019
+ Title: L1: Set Alarm
+ Kata: https://www.codewars.com/kata/l1-set-alarm/javascript
+ */
+
+function setAlarm(employed, vacation)
+{
+ return employed && !vacation;
+}
